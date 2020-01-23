28929772c596f2b52f8b65771a591801a016eb59
Karen Quan <quan.karen@gmail.com>
2020-01-23 08:21:40 -0800

    Add build folder

---
+++ client/build/precache-manifest.89bf5855dfb474cf0a54e2309151f32e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f86dd162409e91d3d4c4c7526f92b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f8363f3307d1d47753",
+    "url": "/static/css/main.5a829247.chunk.css"
+  },
+  {
+    "revision": "795b16b5b284ebae241b",
+    "url": "/static/js/2.428448b8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.428448b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59f8363f3307d1d47753",
+    "url": "/static/js/main.9ce48d36.chunk.js"
+  },
+  {
+    "revision": "2dc53be98f4f784e3b27",
+    "url": "/static/js/runtime-main.35279a7f.js"
+  },
+  {
+    "revision": "31b860ff108378c5e48a44a6d44b4060",
+    "url": "/static/media/boba.31b860ff.svg"
+  },
+  {
+    "revision": "b2b1a0f997bd9581d73990c076511829",
+    "url": "/static/media/empty-boba.b2b1a0f9.svg"
+  },
+  {
+    "revision": "6dac08a6d8ba44759aaee9fbc04b6dd3",
+    "url": "/static/media/lora-regular-webfont.6dac08a6.woff"
+  },
+  {
+    "revision": "f3e78bc5ee8ae9c38c78badca089b55f",
+    "url": "/static/media/lora-regular-webfont.f3e78bc5.woff2"
+  },
+  {
+    "revision": "56f4f534d33907656fee7624b100b77b",
+    "url": "/static/media/montserrat-light-webfont.56f4f534.woff2"
+  },
+  {
+    "revision": "9a7e1affe4cc630afd86aff29ce1ea4c",
+    "url": "/static/media/montserrat-light-webfont.9a7e1aff.woff"
+  },
+  {
+    "revision": "92e54b333702aab3ccb01f0aa3a7a926",
+    "url": "/static/media/remove-boba.92e54b33.gif"
+  }
+]);
