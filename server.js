97792bba8bb7707931c062587630ab7ec5ef5934
Karen Quan <quan.karen@gmail.com>
2020-01-23 10:18:59 -0800

    Update server

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ app.get("/api/businesses", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   // app.use("/", express.static(path.join(__dirname, "/client/build")));
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  // app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static("client/build"));
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
     res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
