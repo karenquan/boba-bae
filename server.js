197e76c051be34e1ed591e25c61d987d003b439c
Karen Quan <quan.karen@gmail.com>
2020-01-23 18:36:22 -0800

    Update res.sendFile path

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
